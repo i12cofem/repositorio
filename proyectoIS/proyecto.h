dd2843fac48bd84c39c785171807f58001dcb738
LEGION <LEGION@192.168.56.1>
2022-12-01 18:58:20 +0100

    Actualización.h 01/12/2022 18:59

--- proyectoIS/proyecto.h
+++ proyectoIS/proyecto.h
@@ -9,8 +9,6 @@ using namespace std;
 #define	CURSO_H
 
 
-
-
 class Usuarios{
 
 	private:
@@ -28,11 +26,21 @@ class Usuarios{
 		void mostrar_cursos();
 		void estadisticas();
 
+	//Getters
+
 	inline string get_dni(){return dni_;};
 	inline string get_nombre(){return nombre_;};
 	inline string get_apellido(){return apellido_;};
 	inline string get_correoUco(){return correo_uco_;};
 	inline string get_password(){return password_;};
+
+	//Setters
+
+	inline string set_dni(string dni){return dni_ = dni;};
+	inline string set_nombre(string nombre){return nombre_ = nombre;};
+	inline string set_apellido(string apellido){return apellido_ = apellido;};
+	inline string set_correo_uco(string correo_uco){return correo_uco_ = correo_uco;};
+	inline string set_password(string password){return password_ = password;};
 };
 
 
@@ -75,8 +83,8 @@ class Cursos{
 	private:
 		int idC_;
 		string nombrecurso_;
-		string fechaInicio;
-		string fechaFin;
+		string fechaInicio_;
+		string fechaFin_;
 		list<Usuarios>lista_espera_;
 		string asignatura_;
 		string recursos_;
@@ -88,6 +96,28 @@ class Cursos{
 		string mostrar_informacion();
 		string ver_asignaturas();
 		string ver_profesor();
+
+		//Getters
+
+		inline int get_idC(){return idC_;};
+		inline string get_nombreCurso(){return nombrecurso_;};
+		inline string get_fechaInicio(){return fechaInicio_;};
+		inline string get_fechaFin(){return fechaFin_;};
+		inline string get_asignatura(){return asignatura_;};
+		inline string get_recursos(){return descripcion_;};
+
+		//Setters
+
+		inline int set_idC(int idC){return idC_ = idC;};
+        inline string set_nombreCurso(string nombrecurso){return nombrecurso_ = nombrecurso;};
+		inline string set_fechaInicio(string fechaInicio){return fechaInicio_ = fechaInicio;};
+        inline string set_fechaFin(string fechaFin){return fechaFin_ = fechaFin;};
+        inline string set_asignatura(string asignatura){return asignatura_ = asignatura;};
+        inline string set_recursos(string recursos){return recursos_ = recursos;};
+        inline string set_descripcion(string descripcion){return descripcion_ = descripcion;};
+
+
+
 
 };
 
@@ -101,8 +131,13 @@ class Visitante{
 		string ver_cursos();
 		bool registrarse();
 
+	//Getters
+
 	inline int get_idVisitante(){return idV_;};
 
+	//Setters
+
+	inline int set_idVisitante(int idV){return idV_ = idV;};
 
 };
 
